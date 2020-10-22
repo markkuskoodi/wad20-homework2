4d13babd96d384764145e7c7687aff04b5261afe
koodi <markkus999@outlook.com>
2020-10-22 21:27:47 +0300

    Drop-down user-info improvement

--- res/js/script.js
+++ res/js/script.js
@@ -1,11 +1,17 @@
 $(function(){
-    $('.avatar-container').click(function (){
-        $('.user-info').slideToggle("fast");
-    });
-    $(document).on("click", function(event){
-       var $trigger = $('.avatar-container');
-       if($trigger !== event.target && !$trigger.has(event.target).length){
-           $('.user-info').slideUp("fast");
-       }
+    $(document).ready(function(){
+        $(document).on("click", function(event){
+            if(!$('.user-info').is(":hidden")) {
+                var target = $(event.target)
+                const $trigger = $(".avatar");
+                if (!$trigger.is(target) && !target.is($('.user-info')) && target.parents('.user-info').length !== 1) {
+                    $('.user-info').slideUp("fast");
+                }
+
+            }
+        });
+        $("body").on('click', '.avatar', function(){
+            $(".user-info").slideToggle("fast");
+        });
     });
 });
