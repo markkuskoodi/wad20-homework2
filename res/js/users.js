cee078287e0e10141df9c204305211bb09b99b54
Elina Meier <elina.meier@ut.ee>
2020-10-25 12:45:13 +0200

    load users in browse

--- res/js/users.js
+++ res/js/users.js
@@ -1,11 +1,24 @@
 $(function () {
     loadUsersInfo()
-        .then(function (user) {
-            displayUsers(user)
+        .then(function () {
+            loadUsersInfo()
         })
         .catch(function () {
             alert('Error loading user info')
         });
+
+    let clicked = false;
+    $(document).on("click", '.follow-button', function(){
+        if(!clicked) {
+            $(this).css('background-color', '#590f6d');
+            clicked = true
+        }
+        else{
+            $(this).css('background-color', '#fffefe')
+            // $(this).css('border', ) sama lillaga tuleb border
+            clicked = false
+        }
+    })
 });
 
 function loadUsersInfo() {
@@ -13,20 +26,24 @@ function loadUsersInfo() {
         {
             url: 'https://private-anon-70efa6a1a7-wad20postit.apiary-mock.com/profiles',
             success: function (response) {
-
                 for (let user of response) {
-                    return user;
-                }
+                    let div = $('<div class="user">');
+                    let avatar = $('<img alt="Avatar">').attr('src', user.avatar);
+                    let name = $('<small>').text(user.firstname + " " + user.lastname);
+                    let actions = $('<div class="user-actions">');
+                    let follow = $('<button type="button" name="follow" class="follow-button">').text("Follow");
 
+                    div.append(avatar);
+                    div.append(name);
+                    div.append(actions);
+                    actions.append(follow);
+
+                    $('users').append(div);
+                }
             },
             error: function () {
                 alert('error')
             }
         }
     )
-}
-
-function displayUsers(user) {
-    $('#name').text(user.firstname + " " + user.lastname);
-    $('.avatar').attr({"src": user.avatar});
 }
